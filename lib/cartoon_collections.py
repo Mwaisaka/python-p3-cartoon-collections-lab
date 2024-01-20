cb5197efd20ac47a429c9cd492181a6069f6253f
Frank <frankkashere@gmail.com>
2024-01-20 20:12:07 +0300

    Completed assignment

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,22 @@
-def roll_call_dwarves():
-    pass
 
-def summon_captain_planet():
-    pass
+cheeses = ["camembert", "gouda", "cheddar"]
+def roll_call_dwarves(dwarf):
+    x=1
+    for name in dwarf:
+        print(f'{x}. {name}')
+        x+=1
 
-def long_planeteer_calls():
-    pass
+def summon_captain_planet(planteers):
+    return [f'{element.title()}!' for element in planteers]
 
-def find_the_cheese():
-    pass
+def long_planeteer_calls(calls_list):
+    for call in calls_list:
+        if len(call)>4:
+            return True
+    return False
+
+def find_the_cheese(foods):
+    for food in foods:
+        if food in cheeses:
+            return food
+    return None
